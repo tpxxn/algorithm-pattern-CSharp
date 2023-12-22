a240d8a1c0d7ffaff90215041ad9fd0a1c1aa1d5
tpxxn <351765204@qq.com>
2023-12-22 17:14:05 +0800

    [chore]回溯法单元测试使用集合等价判断CollectionAssert.AreEquivalent

--- UnitTest/advanced_algorithm/BacktrackTest.cs
+++ UnitTest/advanced_algorithm/BacktrackTest.cs
@@ -13,122 +13,73 @@ public class BacktrackTest
     public void SubsetsTest()
     {
         var result = new Backtrack_Subsets().Subsets(new int[] { 1, 2, 3 });
-        Assert.That(result, Is.EqualTo(new List<List<int>>
+        CollectionAssert.AreEquivalent(new List<List<int>>
         {
             new List<int>
             {
                 Capacity = 0
             },
-            new List<int> { 3 },
-            new List<int> { 2 },
-            new List<int> { 2, 3 },
             new List<int> { 1 },
-            new List<int> { 1, 3 },
+            new List<int> { 2 },
             new List<int> { 1, 2 },
+            new List<int> { 3 },
+            new List<int> { 1, 3 },
+            new List<int> { 2, 3 },
             new List<int> { 1, 2, 3 },
-        }));
+        }, result);
     }
 
     [Test]
     public void SubsetsTest_2()
     {
         var result = new Backtrack_Subsets().Subsets(new int[] { 0 });
-        Assert.That(result, Is.EqualTo(new List<List<int>>
+        CollectionAssert.AreEquivalent(new List<List<int>>
         {
             new List<int>
             {
                 Capacity = 0
             },
             new List<int> { 0 },
-        }));
+        }, result);
     }
 
     [Test]
     public void SubsetsWithDupTest()
     {
         var result = new Backtrack_SubsetsWithDup().SubsetsWithDup(new int[] { 1, 2, 2 });
-        Assert.That(result, Is.EqualTo(new List<List<int>>
+        CollectionAssert.AreEquivalent(new List<List<int>>
         {
             new List<int>
             {
                 Capacity = 0
             },
-            new List<int> { 2 },
-            new List<int> { 2, 2 },
             new List<int> { 1 },
             new List<int> { 1, 2 },
             new List<int> { 1, 2, 2 },
-        }));
+            new List<int> { 2 },
+            new List<int> { 2, 2 },
+        }, result);
     }
 
     [Test]
     public void SubsetsWithDupTest_2()
     {
         var result = new Backtrack_SubsetsWithDup().SubsetsWithDup(new int[] { 0 });
-        Assert.That(result, Is.EqualTo(new List<List<int>>
+        CollectionAssert.AreEquivalent(new List<List<int>>
         {
             new List<int>
             {
                 Capacity = 0
             },
             new List<int> { 0 },
-        }));
+        }, result);
     }
 
     [Test]
     public void PermuteTest()
     {
         var result = new Backtrack_Permute().Permute(new int[] { 1, 2, 3 });
-        Assert.That(result, Is.EqualTo(new List<List<int>>
-        {
-            new List<int> { 1, 2, 3 },
-            new List<int> { 1, 3, 2 },
-            new List<int> { 2, 1, 3 },
-            new List<int> { 2, 3, 1 },
-            new List<int> { 3, 2, 1 },
-            new List<int> { 3, 1, 2 }
-        }));
-    }
-
-    [Test]
-    public void PermuteTest_2()
-    {
-        var result = new Backtrack_Permute().Permute(new int[] { 0, 1 });
-        Assert.That(result, Is.EqualTo(new List<List<int>>
-        {
-            new List<int> { 0, 1 },
-            new List<int> { 1, 0 }
-        }));
-    }
-
-    [Test]
-    public void PermuteTest_3()
-    {
-        var result = new Backtrack_Permute().Permute(new int[] { 1 });
-        Assert.That(result, Is.EqualTo(new List<List<int>>
-        {
-            new List<int> { 1 }
-        }));
-    }
-
-
-    [Test]
-    public void PermuteUniqueTest()
-    {
-        var result = new Backtrack_PermuteUnique().PermuteUnique(new int[] { 1, 1, 2 });
-        Assert.That(result, Is.EqualTo(new List<List<int>>
-        {
-            new List<int> { 1, 1, 2 },
-            new List<int> { 1, 2, 1 },
-            new List<int> { 2, 1, 1 }
-        }));
-    }
-
-    [Test]
-    public void PermuteUniqueTest_2()
-    {
-        var result = new Backtrack_PermuteUnique().PermuteUnique(new int[] { 1, 2, 3 });
-        Assert.That(result, Is.EqualTo(new List<List<int>>
+        CollectionAssert.AreEquivalent(new List<List<int>>
         {
             new List<int> { 1, 2, 3 },
             new List<int> { 1, 3, 2 },
@@ -136,47 +87,96 @@ public class BacktrackTest
             new List<int> { 2, 3, 1 },
             new List<int> { 3, 1, 2 },
             new List<int> { 3, 2, 1 }
-        }));
+        }, result);
+    }
+
+    [Test]
+    public void PermuteTest_2()
+    {
+        var result = new Backtrack_Permute().Permute(new int[] { 0, 1 });
+        CollectionAssert.AreEquivalent(new List<List<int>>
+        {
+            new List<int> { 0, 1 },
+            new List<int> { 1, 0 }
+        }, result);
+    }
+
+    [Test]
+    public void PermuteTest_3()
+    {
+        var result = new Backtrack_Permute().Permute(new int[] { 1 });
+        CollectionAssert.AreEquivalent(new List<List<int>>
+        {
+            new List<int> { 1 }
+        }, result);
+    }
+
+
+    [Test]
+    public void PermuteUniqueTest()
+    {
+        var result = new Backtrack_PermuteUnique().PermuteUnique(new int[] { 1, 1, 2 });
+        CollectionAssert.AreEquivalent(new List<List<int>>
+        {
+            new List<int> { 1, 1, 2 },
+            new List<int> { 1, 2, 1 },
+            new List<int> { 2, 1, 1 }
+        }, result);
+    }
+
+    [Test]
+    public void PermuteUniqueTest_2()
+    {
+        var result = new Backtrack_PermuteUnique().PermuteUnique(new int[] { 1, 2, 3 });
+        CollectionAssert.AreEquivalent(new List<List<int>>
+        {
+            new List<int> { 1, 2, 3 },
+            new List<int> { 1, 3, 2 },
+            new List<int> { 2, 1, 3 },
+            new List<int> { 2, 3, 1 },
+            new List<int> { 3, 1, 2 },
+            new List<int> { 3, 2, 1 }
+        }, result);
     }
 
     [Test]
     public void CombinationSumTest()
     {
         var result = new Backtrack_CombinationSum().CombinationSum(new int[] { 2, 3, 6, 7 }, 7);
-        Assert.That(result, Is.EqualTo(new List<List<int>>
+        CollectionAssert.AreEquivalent(new List<List<int>>
         {
             new List<int> { 2, 2, 3 },
             new List<int> { 7 }
-        }));
+        }, result);
     }
 
     [Test]
     public void CombinationSumTest_2()
     {
         var result = new Backtrack_CombinationSum().CombinationSum(new int[] { 2, 3, 5 }, 8);
-        Assert.That(result, Is.EqualTo(new List<List<int>>
+        CollectionAssert.AreEquivalent(new List<List<int>>
         {
             new List<int> { 2, 2, 2, 2 },
             new List<int> { 2, 3, 3 },
             new List<int> { 3, 5 }
-        }));
+        }, result);
     }
 
     [Test]
     public void CombinationSumTest_3()
     {
         var result = new Backtrack_CombinationSum().CombinationSum(new int[] { 2 }, 1);
-        Assert.That(result, Is.EqualTo(new List<List<int>>
+        CollectionAssert.AreEquivalent(new List<List<int>>
         {
             Capacity = 0
-        }));
+        }, result);
     }
 
     [Test]
     public void LetterCombinationsTest()
     {
         var result = new Backtrack_LetterCombinations().LetterCombinations("23");
-        Assert.That(result, Is.EqualTo(new List<string>
+        CollectionAssert.AreEquivalent(new List<string>
         {
             "ad",
             "ae",
@@ -187,28 +187,28 @@ public class BacktrackTest
             "cd",
             "ce",
             "cf"
-        }));
+        }, result);
     }
 
     [Test]
     public void LetterCombinationsTest_2()
     {
         var result = new Backtrack_LetterCombinations().LetterCombinations("");
-        Assert.That(result, Is.EqualTo(new List<string>
+        CollectionAssert.AreEquivalent(new List<string>
         {
             Capacity = 0
-        }));
+        }, result);
     }
 
     [Test]
     public void LetterCombinationsTest_3()
     {
         var result = new Backtrack_LetterCombinations().LetterCombinations("2");
-        Assert.That(result, Is.EqualTo(new List<string>
+        CollectionAssert.AreEquivalent(new List<string>
         {
             "a",
             "b",
             "c"
-        }));
+        }, result);
     }
 }
